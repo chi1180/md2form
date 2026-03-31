4963945b2e0237eaae0b8f1f5fb5f4b43344990c
chi1180 <143685104+chi1180@users.noreply.github.com>
2026-03-31 20:04:30 +0900

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,9 +2,23 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
-    runtime: "edge",
     override: {
-      // KVなどの設定がない場合、これらを "dummy" に設定する必要があります
+      // Cloudflare Workersの最新のNode.js互換機能を利用するための指定
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  // middlewareを使用していない場合でも、バリデーションを通すために定義が必要な場合があります
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
