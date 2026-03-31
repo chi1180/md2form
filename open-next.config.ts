e46b064f9bdf11fb18328ec4f66ac681391a8fa0
chi1180 <143685104+chi1180@users.noreply.github.com>
2026-03-31 19:59:22 +0900

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,6 +3,12 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 const config: OpenNextConfig = {
   default: {
     runtime: "edge",
+    override: {
+      // KVなどの設定がない場合、これらを "dummy" に設定する必要があります
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
   },
 };
 
