62fd8464622aa9bed54c8f16a9aedd9ce9838b39
chi1180 <143685104+chi1180@users.noreply.github.com>
2026-04-18 11:07:55 +0900

    AEO fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,32 @@ import { Inter, Noto_Sans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 const googleVerification = process.env.GOOGLE_SITE_VERIFICATION ?? "NO_TOKEN";
 
+const GAID = process.env.GAID;
+
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Md2Form",
   description:
     "Write forms in Markdown, share instantly, and analyze responses.",
+  keywords: ["form", "markdown", "share", "analyze"],
   verification: {
     google: googleVerification,
+  },
+  openGraph: {
+    type: "website",
+    url: defaultUrl,
+    title: "Md2Form",
+    description:
+      "Write forms in Markdown, share instantly, and analyze responses.",
   },
 };
 
@@ -53,6 +66,9 @@ export default function RootLayout({
           {children}
           <Toaster richColors position="top-right" />
         </ThemeProvider>
+        {GAID && <GoogleAnalytics gaId={GAID} />}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
